2f349db268ff8282ab820c4d2a2466c9faa32b1e
jsargueta <jrargueta17@gmail.com>
2025-11-01 19:04:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-# app.py
 import streamlit as st
+import mysql.connector
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 
-# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
-mostrar_venta()
