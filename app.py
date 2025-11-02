76babd3dc65e1a29414c758eb45289dbf3a2c669
jsargueta <jrargueta17@gmail.com>
2025-11-01 19:09:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-import streamlit as st
+import streamlit   as st
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 
 # Llamamos a la función mostrar_venta para mostrar el mensaje en la app
